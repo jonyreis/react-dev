a58152ea41e407677fe1bd9d5c385e1af22133d2
Jony <jonyreiscardoso@gmail.com>
2021-02-13 12:02:29 -0400

    feat: ensure RemoteAuthentication calls HttpPostClient with correct body

--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,10 +1,10 @@
 import { AccountModel } from '../models/account-model'
 
-type AthenticationParams = {
+export type AuthenticationParams = {
   email: string
   password: string
 }
 
 export interface Authentication {
-  auth(params: AthenticationParams): Promise<AccountModel>
+  auth(params: AuthenticationParams): Promise<AccountModel>
 }
